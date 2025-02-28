9106e2c7760c29d6c358a2fd3bc17940a03fe4ef
hixdann <danilo.f.almeida@outlook.com>
2025-02-28 18:32:45 -0300

    Adicionado cores e personalização de home

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,19 +2,24 @@ function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4
-            			   bg-yellow-500 text-black'>
+            			   bg-bege-2 text-black'>
             
                 <div className="container flex justify-between text-lg">
-                    Prato Certo
-
-                    <div className='flex gap-4'>
-                        Categorias
-                        Produtos
-                        Vegetarianos
-                        Veganos
-                        Restaurantes
-                        Login
-                        Sair
+                    <div className= "flex items-center p-2" >
+                        <img width="40px" height="40px" src="/src/assets/img/logo/logo_puro.svg" className="self-end"/>
+                        <h2 className="font-bold self-end">
+                            <span className=" text-black">PRATO</span>
+                            <span className="text-verde-3">CERTO</span>
+                        </h2>
+                    </div>
+                    <div className='flex gap-4 p-4'>
+                        <span>Categorias</span>
+                        <span>Produtos</span>
+                        <span>Vegetarianos</span>
+                        <span>Veganos</span>
+                        <span>Restaurantes</span>
+                        <span>Login</span>
+                        <span>Sair</span>
                     </div>
                 </div>
             </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,15 +2,18 @@ function Home() {
     return (
         <>
             <div className="w-screen flex justify-center">
-                <div>
-                    <div className="max-w-7xl flex flex-col items-center">
-                        <h2>Seja Bem Vinde!</h2>
-                        <p>Expresse aqui seus pensamentos e opniões</p>
+            <div className="grid grid-cols-1 md:grid-cols-4 grid-rows-4 gap-8 p-8">
+                    <div className="max-w-7xl col-start-1 col-span-2 row-span-3">
+                        <h1 className="text-4xl">PratoCerto!</h1>
+                        <h2 className="text-3xl text-verde-1">Encontrar opções saudáveis nunca foi tão fácil</h2>
+                        <p className="text-2xl ">Facilitamos a busca por opções saudáveis. Encontre alimentos 
+                            nutritivos, informações confiáveis sobre bem-estar e 
+                            recursos para um lifestyle equilibrado, tudo em um só lugar.</p>
                     </div>
 
-                    <div className="max-w-7xl flex flex-col items-center">
+                    <div className="col-start-3 col-span-2 row-span-3">
                         <img
-                            src="https://i.imgur.com/VpwApCU.png"
+                            src="/src/assets/img/background/fundo_home.png"
                             alt="Imagem da Página Home"
                             width="400px"
                         />
