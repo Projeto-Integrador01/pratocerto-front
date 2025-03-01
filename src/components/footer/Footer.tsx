a263472a9c89e40e4a3bb6f42ea431bfac3a96f7
hixdann <danilo.f.almeida@outlook.com>
2025-03-01 00:39:05 -0300

    Ajustes de estilos na Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,5 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { Envelope } from '@phosphor-icons/react/dist/ssr'
 
 function Footer() {
 
@@ -6,20 +7,59 @@ function Footer() {
 
     return (
         <>
-            <div className="flex justify-center bg-yellow-700 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Prato Certo codArte | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
-                        Sobre Nós
-                    </div>
-                </div>
+            <div className="bg-verde-1 text-white p-4">
+        <div className="container mx-auto py-8">
+        {/* Seção superior com os textos */}
+        <div className="flex justify-between items-start pb-4 text-bege-2">
+            <div className="flex flex-col">
+                <p className="text-lg">| Sobre nós</p>
+                <p className="text-lg">| Nosso Objetivo</p>
+                <p className="text-lg">| Agradecimentos</p>
+                <p className="text-lg">| Fale Conosco</p>
             </div>
+            <div className="flex items-center gap-4">
+                <img
+                    width="100px"
+                    src="/src/assets/img/logo/logo_gen.svg"
+                    alt="Generation"
+                    className="self-start"
+                />
+            </div>
+        </div>
+
+        {/* Linha horizontal fina */}
+        <div className="border-t border-bege-2 my-4"></div>
+
+        {/* Rodapé com o logo do "Prato Certo" */}
+        <div className="flex justify-between items-center">
+            <div className="flex items-center gap-4">
+                <img
+                    width="40px"
+                    height="40px"
+                    src="/src/assets/img/logo/logo_puro_claro.svg" className="self-start"
+                    alt="Prato Certo Logo"
+                />
+                <div className="flex flex-col items-start space-y-0,5">
+                <h2 className="font-bold">
+                    <span className="text-white">PRATO</span>
+                    <span className="text-bege-2">CERTO</span> 
+                </h2>
+                <span className='text-sm text-white'>© 2025 codArte</span>
+            </div>
+        </div>
+            {/* Logos das redes sociais */}
+            <div className="flex flex-col items-start space-y-0,5">
+                <p className="text-lg text-bege-2">Redes Sociais</p>
+            <div className="flex gap-4">
+                <Envelope size={32} weight="thin" className='text-bege-2' />
+                <InstagramLogo size={32} weight="thin" className='text-bege-2'/>
+                <GithubLogo size={32} weight="thin" className='text-bege-2'/>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
         </>
     )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,7 @@ function Home() {
     return (
         <>
             <div className="w-screen flex justify-center">
-            <div className="grid grid-cols-1 md:grid-cols-4 grid-rows-4 gap-8 p-8">
+            <div className="grid grid-cols-1 md:grid-cols-5 grid-rows-4 gap-8 p-8">
                     <div className="max-w-7xl col-start-1 col-span-2 row-span-3">
                         <h1 className="text-4xl">PratoCerto!</h1>
                         <h2 className="text-3xl text-verde-1">Encontrar opções saudáveis nunca foi tão fácil</h2>
@@ -11,7 +11,7 @@ function Home() {
                             recursos para um lifestyle equilibrado, tudo em um só lugar.</p>
                     </div>
 
-                    <div className="col-start-3 col-span-2 row-span-3">
+                    <div className="col-start-4 col-span-2 row-span-3">
                         <img
                             src="/src/assets/img/background/fundo_home.png"
                             alt="Imagem da Página Home"
