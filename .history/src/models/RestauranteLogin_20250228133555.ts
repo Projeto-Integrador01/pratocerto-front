2fd136790785b63e56936da973820d55532376ef
GustavoCorrea10 <guganow11@gmail.com>
2025-03-01 01:49:46 -0300

    removendo historico

--- .history/src/models/RestauranteLogin_20250228133555.ts
+++
@@ -1,9 +0,0 @@
-export default interface RestauranteLogin {
-    id: number;
-    nome: string;
-    usuario: string;
-    senha: string;
-    foto: string;
-    endereco:string;
-    token: string;
-}
