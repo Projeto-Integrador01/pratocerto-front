b5bba50dd74d1b4b2ada347319727d9cca5660ac
Rosilene Farias <rosilene.farias00@gmail.com>
2025-03-03 02:02:10 -0300

    arrumando o tamanho dos cards

--- src/components/categoria/cardcategoria/CardCategoria.tsx
+++ src/components/categoria/cardcategoria/CardCategoria.tsx
@@ -7,24 +7,24 @@ interface CardCategoriasProps {
 
 function CardCategoria({ categoria }: CardCategoriasProps) {
   return (
-    <div className="border-2 border-green-800 rounded-2xl overflow-hidden shadow-md bg-[#D1B890] w-80 p-3 flex flex-col items-center transition duration-300 hover:shadow-xl">
+    <div className="border border-green-800 rounded-xl overflow-hidden shadow-md bg-[#D1B890] w-60 p-2 flex flex-col items-center transition duration-300 hover:shadow-xl">
       {/* Imagem */}
-      <div className="w-full rounded-xl overflow-hidden">
+      <div className="w-full h-32 rounded-lg overflow-hidden">
         {categoria.foto ? (
           <img
             src={categoria.foto}
             alt="Categoria"
-            className="w-full h-48 object-cover rounded-xl"
+            className="w-full h-full object-cover rounded-lg"
           />
         ) : (
-          <div className="w-full h-48 flex items-center justify-center bg-[#A8A878]">
-            <span className="text-white text-lg font-medium">Sem imagem</span>
+          <div className="w-full h-full flex items-center justify-center bg-[#A8A878]">
+            <span className="text-white text-sm font-medium">Sem imagem</span>
           </div>
         )}
       </div>
 
       {/* Nome da Categoria */}
-      <p className="text-[#2F4F2F] text-xl font-bold text-center my-4 uppercase">
+      <p className="text-[#2F4F2F] text-lg font-bold text-center my-2 uppercase">
         {categoria.nome}
       </p>
 
@@ -32,14 +32,14 @@ function CardCategoria({ categoria }: CardCategoriasProps) {
       <div className="flex w-full">
         <Link
           to={`/editarcategoria/${categoria.id}`}
-          className="w-1/2 text-white bg-[#5A7D5A] hover:bg-[#466046] py-2 text-center font-medium rounded-bl-xl transition duration-200"
+          className="w-1/2 text-white bg-[#5A7D5A] hover:bg-[#466046] py-1 text-center text-sm font-medium rounded-bl-lg transition duration-200"
         >
           Editar
         </Link>
 
         <Link
           to={`/deletarcategoria/${categoria.id}`}
-          className="w-1/2 text-white bg-[#B85042] hover:bg-[#92372E] py-2 text-center font-medium rounded-br-xl transition duration-200"
+          className="w-1/2 text-white bg-[#B85042] hover:bg-[#92372E] py-1 text-center text-sm font-medium rounded-br-lg transition duration-200"
         >
           Deletar
         </Link>
